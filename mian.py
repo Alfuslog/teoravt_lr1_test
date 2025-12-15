ab6232261550ba76fe309f8bb6e18e18368f54b6
lkmqdoge <qdoge2335@gmail.com>
2025-12-15 17:32:55 +0300

    куски для транспортной

--- mian.py
+++ mian.py
@@ -296,6 +296,8 @@ class TransportationTable:
                 if transportation.quantity != '*':
                     transportations_cost += transportation.cost * transportation.quantity
         return int(transportations_cost)
+
+
 def print_matrix(matrix):
     for row in matrix:
         print(' '.join(f"{x:5}" for x in row))
